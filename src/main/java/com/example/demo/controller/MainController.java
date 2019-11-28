bfd1ebe1ceefcfd3129110c3d3b666ed7e491ab1
dev <dev@192.168.0.6>
2019-11-28 21:51:26 +0530

    Added controller to databse

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,51 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.ResponseObject;
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
 
 @RestController
 public class MainController {
+	
+	@Autowired
+	private UserRepo repo;
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String show() {
 		return "welcome in my fitst deployment ";
 	}
-
+	
+	@RequestMapping(value="save",method=RequestMethod.POST)
+	public ResponseObject savedata(@RequestParam("name") String name,@RequestParam("address")String address) {
+		ResponseObject res = new ResponseObject();
+	    User user = new User();
+	    user.setName("devesh");
+	    user.setAddress("mishra");
+	   User saved =  repo.save(user);
+	    if(saved!=null) {
+	    	res.setHasError(false);
+	    	res.setMessage("data saved");
+	    	return res;
+	    }
+	    else {
+	    	res.setMessage("some problem");
+		return res;
+	}	
+	}
+	
+	@GetMapping("getAll")
+	public List<User> showAll(){
+			List<User> user =	repo.findAll();
+			System.out.println(user);
+			return user;
+	}
 }
